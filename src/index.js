f6ed132d3a3d61cfbab618d5b95c477052bcd62c
omtiwari52 <tiwariom900@gmail.com>
2025-03-06 02:30:34 +0530

    [Om Tiwari] add: Ability to sort entries according to the city name and person name feature added✨

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 const AddressBook = require("./service/AddressBook.js");
 const Contact = require("./model/Contact.js");
 
-const myAddressBook = new AddressBook();
+const addressBook = new AddressBook();
 
 try {
-    const contact1 = new Contact(
-        "Omi", 
-        "Tiwari",
-        "Anand Nagar",
-        "Satna City",
-        "Madhya Pradesh", 
-        "485001",  
-        "8967342980", 
-        "omtiwari2009@gmail.com"
-    );
+    addressBook.addContact({ firstName: "Omi", lastName: "Tiwari", address:"Anand Nagar",city: "Satna", state: "Madhya Pradesh", zip: 485001 ,phone: 8067354789,email:"omtiwari2009@gmail.com"});
+    addressBook.addContact({ firstName: "Durgesh", lastName: "Kushwaha", address:"Ashoka Garden",city: "Bhopal", state: "Madhya Pradesh", zip: 462001 ,phone:8956329056,email:"durgeshkushwaha@gmail.com" });
+    addressBook.addContact({ firstName: "Bipin", lastName: "sahu", address:"Press Colony",city: "Lucknow", state: "Uttar Pradesh", zip: 60601,phone:9897969504,email:"bipinsahu456@gmail.com" });
+    console.log("Original Entries: ");
     
-    myAddressBook.addContact(contact1);
-    console.log(myAddressBook.getContact());
+    addressBook.getContact().forEach(contact =>{
+        console.log(contact);
+    })
+
+    console.log("Entries after sorting by city:");
+    
+    addressBook.sortByCity();
+    addressBook.getContact().forEach(contact =>{
+        console.log(contact);
+    })
 
     // Sort contacts alphabetically by name
-    const sortedContacts = myAddressBook.sortContactsByName();
-    sortedContacts.forEach(contact => console.log(contact.toString()));
+    console.log("Entries after sorting by name:");
+    
+    const sortedContacts = addressBook.sortContactsByName();
+    sortedContacts.forEach(contact => console.log(contact));
+
+    
 
 } catch (error) {
     console.log(error);
