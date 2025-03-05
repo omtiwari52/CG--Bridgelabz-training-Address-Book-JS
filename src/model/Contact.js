5f84a0856cc31b1c047915cb34c2524e882e6108
omtiwari52 <tiwariom900@gmail.com>
2025-03-05 21:23:28 +0530

    [Om Tiwari] add: Ability to add contacts added✨

---
+++ src/model/Contact.js
@@ -0,0 +1,13 @@
+// Class contact to ensure the following details from the user
+class Contact{
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
